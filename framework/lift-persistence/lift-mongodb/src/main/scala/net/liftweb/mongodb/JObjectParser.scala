230f3481cc29383c059294a81a445f4c7ed2c724
Tim Nelson <tnelly27@gmail.com>
2010-06-29 10:55:47 -0500

    Add full BSON support to lift-mongodb.

--- framework/lift-persistence/lift-mongodb/src/main/scala/net/liftweb/mongodb/JObjectParser.scala
+++ framework/lift-persistence/lift-mongodb/src/main/scala/net/liftweb/mongodb/JObjectParser.scala
@@ -19,12 +19,12 @@ package mongodb {
 
 import scala.collection.JavaConversions._
 
-import _root_.java.util.Date
+import _root_.java.util.{Date, UUID}
+import _root_.java.util.regex.Pattern
 
 import _root_.net.liftweb.json.Formats
 import _root_.net.liftweb.json.JsonAST._
 import _root_.net.liftweb.common.Box
-//import net.liftweb.json.Meta.Reflection._
 
 import com.mongodb.{BasicDBObject, BasicDBList, DBObject}
 import org.bson.types.ObjectId
@@ -34,9 +34,8 @@ object JObjectParser {
   /*
   * Parse a JObject into a DBObject
   */
-  def parse(jo: JObject)(implicit formats: Formats): DBObject = {
+  def parse(jo: JObject)(implicit formats: Formats): DBObject =
     Parser.parse(jo, formats)
-  }
 
   /*
   * Serialize a DBObject into a JObject
@@ -48,9 +47,7 @@ object JObjectParser {
     a.asInstanceOf[AnyRef] match {
       case x if primitive_?(x.getClass) => primitive2jvalue(x)
       case x if datetype_?(x.getClass) => datetype2jvalue(x)(formats)
-      //case x: ObjectId => JString("ObjectId(\""+x.toString+"\")")
-      //case x: ObjectId => JObject(List(JField("jsonClass", JString("ObjectId")), JField("oid", JString(x.toString))))
-      case x: ObjectId => JString(x.toString)
+      case x if mongotype_?(x.getClass) => mongotype2jvalue(x)
       case x: java.util.ArrayList[_] => JArray(x.toList.map( x => serialize(x, formats)))
       case x: Option[_] => serialize(x getOrElse JNothing, formats)
       case x: Box[_] => serialize(x getOrElse JNothing, formats)
@@ -92,21 +89,22 @@ object JObjectParser {
       trimObj(obj).foreach { jf =>
         jf.value match {
           case JArray(arr) => dbo.put(jf.name, parseArray(arr, formats))
-          //case JObject(jo) if (objectId_?(jo)) => dbo.put(jf.name, new ObjectId(jo.last.value.values.toString))
+          case JObject(JField("$oid", JString(s)) :: Nil) if (ObjectId.isValid(s)) =>
+            dbo.put(jf.name, new ObjectId(s))
+          case JObject(JField("$regex", JString(s)) :: JField("$flags", JInt(f)) :: Nil) =>
+            dbo.put(jf.name, Pattern.compile(s, f.intValue))
+          case JObject(JField("$dt", JString(s)) :: Nil) =>
+            formats.dateFormat.parse(s) foreach {
+              d => dbo.put(jf.name, d)
+            }
+          case JObject(JField("$uuid", JString(s)) :: Nil) =>
+            dbo.put(jf.name, UUID.fromString(s))
           case JObject(jo) => dbo.put(jf.name, parseObject(jo, formats))
           case jv: JValue => dbo.put(jf.name, renderValue(jv, formats))
         }
       }
       dbo
     }
-    /*
-    private def objectId_?(obj: List[JField]): Boolean = {
-      (obj.size, obj.first.name, obj.first.value) match {
-        case (2, "jsonClass", JString(s)) if (s == "ObjectId") => true
-        case _ => false
-      }
-    }
-    */
 
     private def renderValue(jv: JValue, formats: Formats): Object = jv match {
       case JBool(b) => java.lang.Boolean.valueOf(b)
@@ -115,17 +113,11 @@ object JObjectParser {
       case JNull => null
       case JNothing => error("can't render 'nothing'")
       case JString(null) => "null"
-      case JString(s) if (ObjectId.isValid(s)) => new ObjectId(s)
-      /*
-      case JString(s) => formats.dateFormat.parse(s) match {
-        case Some(d: Date) => d // match on Date formatted string
-        case _ => s
-      }
-      */
       case JString(s) => s
-      case _ => "match error (renderValue): "+jv.getClass
+      case _ => println("match error (renderValue): "+jv.getClass);  ""
     }
 
+    // FIXME: This is not ideal.
     private def renderInteger(i: BigInt): Object = {
       if (i <= java.lang.Integer.MAX_VALUE && i >= java.lang.Integer.MIN_VALUE) {
         new java.lang.Integer(i.intValue)
